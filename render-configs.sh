099e4eecb67e40ff7c0321940d22fecb564b8b15
Mariko Medlock <marikomedlock@marikomedlock-macbookpro.roam.corp.google.com>
2019-12-18 15:42:44 -0500

    Typo in project_id write out.

--- render-configs.sh
+++ render-configs.sh
@@ -6,4 +6,4 @@ VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
     vault read -format=json secret/dsde/datarepo/dev/sa-key.json | \
     jq .data | tee /tmp/jadecli_client_secret.json | \
-    jq.installed.project_id
+    jq .installed.project_id
