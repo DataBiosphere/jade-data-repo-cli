b64088b1f43e9a46bdd0922dc498abf24be92eda
Mariko Medlock <marikomedlock@marikomedlock-macbookpro.roam.corp.google.com>
2019-12-18 15:04:35 -0500

    Put secret file in /tmp instead.

--- render-configs.sh
+++ render-configs.sh
@@ -4,6 +4,5 @@ LOCAL_TOKEN=$(cat ~/.vault-token)
 VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
-    mkdir -p ~/.jadecli/client | \
     vault read -format=json secret/dsde/datarepo/dev/sa-key.json | \
-    jq .data > ~/.jadecli/client/jadecli_client_secret.json
+    jq .data > /tmp/jadecli_client_secret.json
