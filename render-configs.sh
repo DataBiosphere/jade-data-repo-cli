1cb7d070338ece57082cc364083472e90750b732
Mariko Medlock <marikomedlock@marikomedlock-macbookpro.roam.corp.google.com>
2019-12-18 17:20:17 -0500

    Exclude RepositoryAPI tests from :test task for now.

--- render-configs.sh
+++ render-configs.sh
@@ -3,7 +3,7 @@
 LOCAL_TOKEN=$(cat ~/.vault-token)
 VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
-# uncomment this once we have a solution for Travis running integration tests that require authentication
+# TODO: uncomment this once we have a solution for Travis running integration tests that require authentication
 #docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
 #    vault read -format=json secret/dsde/datarepo/dev/cli-key.json | \
 #    jq .data > /tmp/jadecli_client_secret.json
