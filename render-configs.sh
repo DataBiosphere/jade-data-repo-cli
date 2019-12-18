80889b225c9a58c2507b569813739a6f71ac13ed
Mariko Medlock <marikomedlock@marikomedlock-macbookpro.roam.corp.google.com>
2019-12-18 14:41:34 -0500

    Create client dir inside container, not outside.

--- render-configs.sh
+++ render-configs.sh
@@ -3,7 +3,7 @@
 LOCAL_TOKEN=$(cat ~/.vault-token)
 VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
-mkdir -p ~/.jadecli/client
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
+    mkdir -p ~/.jadecli/client \
     vault read -format=json secret/dsde/datarepo/dev/sa-key.json | \
     jq .data > ~/.jadecli/client/jadecli_client_secret.json
