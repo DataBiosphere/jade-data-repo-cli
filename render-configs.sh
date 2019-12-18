3efbfe6ab25199a9aabd420072a135e3dea4b946
Mariko Medlock <marikomedlock@marikomedlock-macbookpro.roam.corp.google.com>
2019-12-18 15:40:54 -0500

    Write out project_id to see if file is written in same format.

--- render-configs.sh
+++ render-configs.sh
@@ -5,4 +5,5 @@ VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
     vault read -format=json secret/dsde/datarepo/dev/sa-key.json | \
-    jq .data > /tmp/jadecli_client_secret.json
+    jq .data | tee /tmp/jadecli_client_secret.json | \
+    jq.installed.project_id
