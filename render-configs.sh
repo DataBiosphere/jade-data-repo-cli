f54d5f2f276fe9863ef974b17ba96bb2836954f9
Mariko Medlock <marikomedlock@marikomedlock-macbookpro.roam.corp.google.com>
2019-12-18 16:04:21 -0500

    Use new cli-key instead of sa-key from Vault.

--- render-configs.sh
+++ render-configs.sh
@@ -4,6 +4,5 @@ LOCAL_TOKEN=$(cat ~/.vault-token)
 VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
-    vault read -format=json secret/dsde/datarepo/dev/sa-key.json | \
-    jq .data | tee /tmp/jadecli_client_secret.json | \
-    jq .installed.project_id
+    vault read -format=json secret/dsde/datarepo/dev/cli-key.json | \
+    jq .data > /tmp/jadecli_client_secret.json
