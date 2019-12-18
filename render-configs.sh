81a2ac1c274cd35c6be275bd78197436693e3b3c
Mariko Medlock <marikomedlock@marikomedlock-macbookpro.roam.corp.google.com>
2019-12-18 14:59:27 -0500

    Try again with mkdir.

--- render-configs.sh
+++ render-configs.sh
@@ -4,6 +4,6 @@ LOCAL_TOKEN=$(cat ~/.vault-token)
 VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
-    mkdir -p ~/.jadecli/client \
+    mkdir -p ~/.jadecli/client | \
     vault read -format=json secret/dsde/datarepo/dev/sa-key.json | \
     jq .data > ~/.jadecli/client/jadecli_client_secret.json
