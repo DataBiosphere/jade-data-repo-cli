9547649046ceaf5bccc93d321acfaa3ca5716f4a
Mariko Medlock <marikomedlock@marikomedlock-macbookpro.roam.corp.google.com>
2020-01-06 10:29:17 -0500

    Cleanup comments in render-configs script.

--- render-configs.sh
+++ render-configs.sh
@@ -3,8 +3,6 @@
 LOCAL_TOKEN=$(cat ~/.vault-token)
 VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
-# write the
-
 # read the client secret from Vault and store in a JSON file under /tmp
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
     vault read -format=json secret/dsde/datarepo/dev/cli-key.json | \
@@ -15,4 +13,5 @@ docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
     jq .data | tee /tmp/jade-dev-account.json | \
     jq -r .private_key > /tmp/jade-dev-account.pem
 
-# write the context.properties file, also under /tmp
+# TODO: write the service account access token to ~/.creds directory
+# TODO: write the context.properties file, also under /tmp
